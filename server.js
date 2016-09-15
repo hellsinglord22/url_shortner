741014abea5a1bcd2fcfd35413b6600061e9fcc5
H <heshamscorpe55@gmail.com>
2016-09-15 04:36:12 +0200

    testing mongo

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 //^_^ Dependencies injection ^_^ // 
 const express = require('express'); 
+const mongo   = require('mongodb').MongoClient; 
 const app     = express();
 
 // ^)^ some constants for my server //
 const PORT = 5000; 
-
+const CONNECTION_STRING = 'mongodb://localhost:27017/url'; 
+const COLLECTION_NAME = 'urls';  
 
 app.get('/', function(request, response) {
-    response.send('shit is working'); 
+
+
+
 }); 
 
 
